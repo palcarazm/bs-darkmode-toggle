15d4596e6911ae325c58700bc3c687a4e607d0c2
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2026-04-03 22:37:26 +0200

    fix(sonar): provide multiple methods instead of using "result" to determine which action to take
    Methods should not contain selector parameters javascript:S2301

--- test/src/TestRunner.js
+++ test/src/TestRunner.js
@@ -14,25 +14,29 @@ export default class TestRunner {
             let $badge = PAGEMODEL.BADGE.clone();
             let $element = $(this).find(PAGEMODEL.TEST_ELEMENT_SELECTOR);
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "State",
-                PLUGIN.checkState($element)
-            );
+            if(PLUGIN.checkState($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "State");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "State");
+            }
 
-            TestRunner.#addToResultBadge($badge, "Root", PLUGIN.checkRoot($element));
+            if(PLUGIN.checkRoot($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "Root");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "Root");
+            }
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "Light Label",
-                PLUGIN.checkLightLabel($element)
-            );
+            if(PLUGIN.checkLightLabel($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "Light Label");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "Light Label");
+            }
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "Dark Label",
-                PLUGIN.checkDarkLabel($element)
-            );
+            if(PLUGIN.checkDarkLabel($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "Dark Label");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "Dark Label");
+            }
 
             $(this).find(PAGEMODEL.BADGE_CONTAINER_SELECTOR).append($badge);
         });
@@ -48,48 +52,55 @@ export default class TestRunner {
             let $element = $(this).find(PAGEMODEL.TEST_ELEMENT_SELECTOR);
             let options = PAGEMODEL.getOptions($(this));
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "State",
-                PLUGIN.checkState($element, options)
-            );
+            if(PLUGIN.checkState($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "State");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "State");
+            }
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "Root",
-                PLUGIN.checkRoot($element, options)
-            );
+            if(PLUGIN.checkRoot($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "Root");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "Root");
+            }
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "Light Label",
-                PLUGIN.checkLightLabel($element, options)
-            );
+            if(PLUGIN.checkLightLabel($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "Light Label");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "Light Label");
+            }
 
-            TestRunner.#addToResultBadge(
-                $badge,
-                "Dark Label",
-                PLUGIN.checkDarkLabel($element, options)
-            );
+            if(PLUGIN.checkDarkLabel($element, options)) {
+                TestRunner.#addSuccessToResultBadge($badge, "Dark Label");
+            } else {
+                TestRunner.#addFailToResultBadge($badge, "Dark Label");
+            }
 
             $(this).find(PAGEMODEL.BADGE_CONTAINER_SELECTOR).append($badge);
         });
     }
 
     /**
-   * Add Result to the badge
-   * @param {jQuery Element} $bagde
-   * @param {String} test
-   * @param {Boolean} result
-   * @private
-   */
-    static #addToResultBadge($badge, test, result) {
-        if (result) {
-            $badge.append($("<span>/span>").text(test + " PASS"));
-        } else {
-            $badge.append($('<span class="fw-bold"></span>').text(test + " FAIL"));
-            $badge.addClass(PAGEMODEL.BADGE_FAIL_CLASS);
-            $badge.removeClass(PAGEMODEL.BADGE_PASS_CLASS);
-        }
+     * Add Success Result to the badge
+     * @param {jQuery Element} $bagde
+     * @param {String} test
+     * @param {Boolean} result
+     * @private
+     */
+    static #addSuccessToResultBadge($badge, test) {
+        $badge.append($("<span>/span>").text(test + " PASS"));
+    }
+
+    /**
+     * Add Fail Result to the badge
+     * @param {jQuery Element} $bagde
+     * @param {String} test
+     * @param {Boolean} result
+     * @private
+     */
+    static #addFailToResultBadge($badge, test) {
+        $badge.append($('<span class="fw-bold"></span>').text(test + " FAIL"));
+        $badge.addClass(PAGEMODEL.BADGE_FAIL_CLASS);
+        $badge.removeClass(PAGEMODEL.BADGE_PASS_CLASS);
     }
 }
