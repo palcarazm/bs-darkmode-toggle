72ed8d7390b6fa5a0797a140a067114092275eb2
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2026-04-04 12:45:28 +0200

    ci: update rollup config

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,15 +1,15 @@
 const terser = require("@rollup/plugin-terser");
 
 module.exports = [
-  {
-    input: "src/js/main.js",
-    output: [
-      {
-        file: "dist/js/bundle.min.js",
-        format: "umd",
-        sourcemap: true,
-        plugins: [terser()]
-      }
-    ]
-  }
+    {
+        input: "./src/js/main.js",
+        output: [
+            {
+                file: "./dist/js/bundle.min.js",
+                format: "umd",
+                sourcemap: true,
+                plugins: [terser()]
+            }
+        ]
+    }
 ];
